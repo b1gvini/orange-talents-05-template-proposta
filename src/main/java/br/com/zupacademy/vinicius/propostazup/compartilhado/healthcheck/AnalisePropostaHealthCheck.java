20000f4e1731968eb9dd0ffef2cfeb1c648a2b75
Vinícius de França <vgf.vinicius@gmail.com>
2021-06-16 15:22:42 -0300

    rodando_prometheus

--- src/main/java/br/com/zupacademy/vinicius/propostazup/compartilhado/healthcheck/AnalisePropostaHealthCheck.java
+++ src/main/java/br/com/zupacademy/vinicius/propostazup/compartilhado/healthcheck/AnalisePropostaHealthCheck.java
@@ -4,19 +4,26 @@ import java.net.Socket;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnalisePropostaHealthCheck implements HealthIndicator {
+	
+	@Value("${health.proposta.url}")
+	private String urlSaude;
+	
+	@Value("${health.proposta.porta}")
+	private int porta;
 
 	@Override
 	public Health health() {
 		Map<String, String> details = new HashMap<>();
 		details.put("Api Externa", "API Análise de Propostas");
-		details.put("URL", "http://localhost:9999/api/solicitacao");
-		try (Socket socket = new Socket(new java.net.URL("http://localhost/api/solicitacao").getHost(), 9999)) {
+		details.put("URL", urlSaude+"/api/solicitacao");
+		try (Socket socket = new Socket(new java.net.URL(urlSaude+"/api/solicitacao").getHost(), porta)) {
         } catch (Exception e) {
             return Health.down().withDetail("error", e.getMessage()).build();
         }
